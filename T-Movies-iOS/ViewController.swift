80a1aab7109d60a6f4a3e11b204ac351c0b76297
Taha Jadid <jadid.taha@waroom.com>
2023-02-27 15:51:43 +0100

    Initial Commit

---
+++ T-Movies-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  T-Movies-iOS
+//
+//  Created by Taha JADID on 27/2/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
