fd79cbb31b1f27229bed0f5b1de61be8679d77a4
tahajadid <tahajadid98@gmail.com>
2023-03-07 00:26:45 +0100

    Add all

---
+++ Pods/Target Support Files/Pods-T-Movies-iOS-T-Movies-iOSUITests/Pods-T-Movies-iOS-T-Movies-iOSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_T_Movies_iOS_T_Movies_iOSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_T_Movies_iOS_T_Movies_iOSUITests
+@end
---
+++ Pods/Target Support Files/Pods-T-Movies-iOSTests/Pods-T-Movies-iOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_T_Movies_iOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_T_Movies_iOSTests
+@end
