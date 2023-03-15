61c1cd50dc03a5466d5ef54e6564ec97446bf81c
tahajadid <tahajadid98@gmail.com>
2023-03-15 10:39:41 +0100

    Add favorite vc

---
+++ T-Movies-iOS/Controllers/FavouriteVC/FavouriteVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteVC.swift
+//  T-Movies-iOS
+//
+//  Created by Taha JADID on 15/3/2023.
+//
+
+import UIKit
+
+class FavouriteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
