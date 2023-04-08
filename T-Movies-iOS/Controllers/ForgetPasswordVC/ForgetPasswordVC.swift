56df6f9bdfe7a6af6a74566bf6ed5ec1c4c69253
tahajadid <tahajadid98@gmail.com>
2023-04-08 21:46:39 +0000

    Add back behavior in forget pwd

--- T-Movies-iOS/Controllers/ForgetPasswordVC/ForgetPasswordVC.swift
+++ T-Movies-iOS/Controllers/ForgetPasswordVC/ForgetPasswordVC.swift
@@ -11,10 +11,21 @@ class ForgetPasswordVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let gestureSwipe = UISwipeGestureRecognizer(target: self, action:  #selector (self.dimissAll (_:)))
+        gestureSwipe.direction = .right
+        self.view.addGestureRecognizer(gestureSwipe)
     }
 
-
+    @objc func dimissAll(_ sender: UISwipeGestureRecognizer){
+        back()
+    }
+    
     @IBAction func backAction(_ sender: UIButton) {
+        back()
+    }
+    
+    func back() {
         let splashVC = SplashVC()
         splashVC.showOnlyLogin = true
         splashVC.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
